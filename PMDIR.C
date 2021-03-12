726e2924092d05e88fbb2062ad85be518fbc85b7
Martin Iturbide <martiniturbide@gmail.com>
2021-03-11 21:36:44 -0500

    1.2

--- PMDIR.C
+++ PMDIR.C
@@ -10,50 +10,56 @@
 
 int main(int argc, char* argv[])
 {
+APIRET rc;
 HOBJECT hObject;
 UCHAR achDirName[CCHMAXPATH]={0},view[16]={0},object[CCHMAXPATH]={0};
 ULONG cbDirPathLen=0,ulDriveMap=0,ulDriveNum=0;
 
 cbDirPathLen=(ULONG)sizeof(achDirName);
-if (DosQueryCurrentDir(ulDriveNum,achDirName,&cbDirPathLen)==NO_ERROR)
+rc=DosQueryCurrentDir(ulDriveNum,achDirName,&cbDirPathLen);
+if (rc==NO_ERROR)
+   rc=DosQueryCurrentDisk(&ulDriveNum,&ulDriveMap);
+if (rc==NO_ERROR)
    {
-   if (DosQueryCurrentDisk(&ulDriveNum,&ulDriveMap)==NO_ERROR)
-      {
-      printf("\nPMDIR [[D]etails | [I]con | [T]ree | [S]ettings]\n");
-      sprintf(object,"%c:\\%s",'A'+ulDriveNum-1,achDirName,argc);
-      printf("Folder-> %s\n",object);
+   printf("\nPMDIR [[D]etails | [I]con | [T]ree | [S]ettings]\n");
+   sprintf(object,"%c:\\%s",'A'+ulDriveNum-1,achDirName,argc);
+   printf("Folder-> %s\n",object);
 
-      strcpy(view,"OPEN=\0");
-      if (argc==1)      
-         strcat(view,"DEFAULT\0");
-      else   
+   strcpy(view,"OPEN=\0");
+   if (argc==1)
+      strcat(view,"DEFAULT\0");
+   else
+      {
+      switch(argv[1][0])
          {
-         switch(argv[1][0])
-            {
-            case 'D':
-            case 'd':
-               strcat(view,"DETAILS\0");
-               break;
-            case 'I':
-            case 'i':
-               strcat(view,"ICON\0");
-               break;
-            case 'T':
-            case 't':
-               strcat(view,"TREE\0");
-               break;
-            case 'S':
-            case 's':
-               strcat(view,"SETTINGS\0");
-               break;
-            default:            
-               strcat(view,"DEFAULT\0");
-            }
+         case 'D':
+         case 'd':
+            strcat(view,"DETAILS\0");
+            break;
+         case 'I':
+         case 'i':
+            strcat(view,"ICON\0");
+            break;
+         case 'T':
+         case 't':
+            strcat(view,"TREE\0");
+            break;
+         case 'S':
+         case 's':
+            strcat(view,"SETTINGS\0");
+            break;
+         default:
+            strcat(view,"DEFAULT\0");
          }
-      if (NULLHANDLE!=(hObject=WinQueryObject(object)))
-         if (WinSetObjectData(hObject,view)==TRUE) /* open object */
-            WinSetObjectData(hObject,view); /* move object to foreground */
       }
+   if (NULLHANDLE!=(hObject=WinQueryObject(object)))
+      if (WinSetObjectData(hObject,view)==TRUE) /* open object */
+         WinSetObjectData(hObject,view); /* move object to foreground */
+   }
+else
+   {
+   printf("\nError: cannot retrieve full path of the current directory (SYS%04u).\n",rc);
+   return 1;
    }
 
 return 0;
--- PMDIR_NL.C
+++ PMDIR_NL.C
@@ -10,50 +10,56 @@
 
 int main(int argc, char* argv[])
 {
+APIRET rc;
 HOBJECT hObject;
 UCHAR achDirName[CCHMAXPATH]={0},view[16]={0},object[CCHMAXPATH]={0};
 ULONG cbDirPathLen=0,ulDriveMap=0,ulDriveNum=0;
 
 cbDirPathLen=(ULONG)sizeof(achDirName);
-if (DosQueryCurrentDir(ulDriveNum,achDirName,&cbDirPathLen)==NO_ERROR)
+rc=DosQueryCurrentDir(ulDriveNum,achDirName,&cbDirPathLen);
+if (rc==NO_ERROR)
+   rc=DosQueryCurrentDisk(&ulDriveNum,&ulDriveMap);
+if (rc==NO_ERROR)
    {
-   if (DosQueryCurrentDisk(&ulDriveNum,&ulDriveMap)==NO_ERROR)
+   printf("\nPMDIR [[B]oomstructuur | [D]etails | [P]ictogrammen | [I]nstellingen]\n");
+   sprintf(object,"%c:\\%s",'A'+ulDriveNum-1,achDirName,argc);
+   printf("Map-> %s\n",object);
+
+   strcpy(view,"OPEN=\0");
+   if (argc==1)
+      strcat(view,"DEFAULT\0");
+   else
       {
-      printf("\nPMDIR [[B]oomstructuur | [D]etails | [P]ictogrammen | [I]nstellingen]\n");
-      sprintf(object,"%c:\\%s",'A'+ulDriveNum-1,achDirName,argc);
-      printf("Map-> %s\n",object);
-   
-      strcpy(view,"OPEN=\0");
-      if (argc==1)      
-         strcat(view,"DEFAULT\0");
-      else   
+      switch(argv[1][0])
          {
-         switch(argv[1][0])
-            {
-            case 'P':
-            case 'p':
-               strcat(view,"ICON\0");
-               break;
-            case 'D':
-            case 'd':
-               strcat(view,"DETAILS\0");
-               break;
-            case 'B':
-            case 'b':
-               strcat(view,"TREE\0");
-               break;
-            case 'I':
-            case 'i':
-               strcat(view,"SETTINGS\0");
-               break;
-            default:            
-               strcat(view,"DEFAULT\0");
-            }
+         case 'P':
+         case 'p':
+            strcat(view,"ICON\0");
+            break;
+         case 'D':
+         case 'd':
+            strcat(view,"DETAILS\0");
+            break;
+         case 'B':
+         case 'b':
+            strcat(view,"TREE\0");
+            break;
+         case 'I':
+         case 'i':
+            strcat(view,"SETTINGS\0");
+            break;
+         default:
+            strcat(view,"DEFAULT\0");
          }
-      if (NULLHANDLE!=(hObject=WinQueryObject(object)))
-         if (WinSetObjectData(hObject,view)==TRUE) /* open object */
-            WinSetObjectData(hObject,view); /* move object to foreground */
       }
+   if (NULLHANDLE!=(hObject=WinQueryObject(object)))
+      if (WinSetObjectData(hObject,view)==TRUE) /* open object */
+         WinSetObjectData(hObject,view); /* move object to foreground */
+   }
+else
+   {
+   printf("\nFout: kan volledige pad van de huidige directory niet opvragen (SYS%04u).\n",rc);
+   return 1;
    }
 
 return 0;
